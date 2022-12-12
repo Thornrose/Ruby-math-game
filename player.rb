9875b34f2ee285a783632ad00c28451da3200073
Cheever Esler <ames.esler@gmail.com>
2022-12-12 15:20:41 -0400

    add more class files, experiment with player class

--- player.rb
+++ player.rb
@@ -1,3 +1,20 @@
+input1 = ARGV
+
 class Player
+  attr_accessor :score
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    self.score = 3
+  end
+
+  def info
+    puts "My name is #{name}!"
+  end
 
 end
+
+player1 = Player.new(input1)
+
+player1.info
